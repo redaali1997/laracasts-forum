63489f767d4e5eef7ec96d56832047faa7c86a24
Reda Ali <reda5201028@gmail.com>
2023-12-19 06:24:18 +0200

    Pest driving posts

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps(['posts'])
+</script>
+
+<template>
+<AppLayout>
+    <ul>
+        <li v-for="post in posts" :key="post.id" v-text="post.title"></li>
+    </ul>
+</AppLayout>
+</template>
+
