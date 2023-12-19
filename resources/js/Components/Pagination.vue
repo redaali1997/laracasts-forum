ddb01b6cef135c6d790c60587ded3accc7dae959
Reda Ali <reda5201028@gmail.com>
2023-12-19 07:12:38 +0200

    Pagination and performance

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div class="flex flex-1 justify-between sm:hidden">
+            <a href="#"
+                class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</a>
+            <a href="#"
+                class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</a>
+        </div>
+        <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+            <div>
+                <p class="text-sm text-gray-700">
+                    Showing
+                    {{ ' ' }}
+                    <span class="font-medium" v-text="meta.from"></span>
+                    {{ ' ' }}
+                    to
+                    {{ ' ' }}
+                    <span class="font-medium" v-text="meta.to"></span>
+                    {{ ' ' }}
+                    of
+                    {{ ' ' }}
+                    <span class="font-medium" v-text="meta.total"></span>
+                    {{ ' ' }}
+                    results
+                </p>
+            </div>
+            <div>
+                <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                    <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
+                    <template v-for="(link, index) in meta.links" :key="index">
+                        <Link :href="link.url" v-if="link.url" aria-current="page" :class="['p-3', {
+                            'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0': !link.active,
+                            'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active
+                        }]"><span v-html="link.label"></span>
+                        </Link>
+                    </template>
+                </nav>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
+import { Link } from '@inertiajs/vue3';
+
+defineProps(['meta'])
+
+const items = [
+    { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
+    { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
+    { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
+]
+</script>
--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,14 +1,18 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import Pagination from '@/Components/Pagination.vue';
+import Container from '@/Components/Container.vue';
 
 defineProps(['posts'])
 </script>
 
 <template>
-<AppLayout>
-    <ul>
-        <li v-for="post in posts" :key="post.id" v-text="post.title"></li>
-    </ul>
-</AppLayout>
+    <AppLayout>
+        <Container>
+            <ul>
+                <li v-for="post in posts.data" :key="post.id" v-text="post.title"></li>
+            </ul>
+            <Pagination :meta="posts.meta"/>
+        </Container>
+    </AppLayout>
 </template>
-
